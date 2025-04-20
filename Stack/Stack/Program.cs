16938f09334e2baa3fb8eb398fef278b345a30b1
Dimitry Rezvyackov <dimrez199@gmail.com>
2025-04-20 22:51:14 +0300

    Stack Homework
    Homework

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace HomeWork
+{
+    public class Stack<T>
+    {
+        private readonly T[] _items;
+        private int _top = -1;
+
+        public Stack(int capacity)
+        {
+            if (capacity <= 0)
+                throw new ArgumentOutOfRangeException(nameof(capacity), "Capacity must be positive");
+
+            _items = new T[capacity];
+            Capacity = capacity;
+        }
+
+        public int Capacity { get; }
+        public int Count => _top + 1;
+        public bool IsEmpty => _top == -1;
+        public bool IsFull => _top == Capacity - 1;
+
+        public bool Push(T item)
+        {
+            if (IsFull)
+                return false;
+
+            _items[++_top] = item;
+            return true;
+        }
+
+        public T? Pop()
+        {
+            if (IsEmpty)
+                return default;
+
+            return _items[_top--];
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Stack is empty");
+
+            return _items[_top];
+        }
+    }
+}
